715dae2586ee945504cedf1ea357ceb9a7804e75
imhonggeun <dkfl9870@gmail.com>
2025-10-02 10:29:10 +0900

    application annotation 추가

--- src/main/java/com/java/authorizationApplication.java
+++ src/main/java/com/java/authorizationApplication.java
@@ -2,8 +2,14 @@ package com.java;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import com.java.config.RsaKeyProperties;
+
+@EnableJpaAuditing
 @SpringBootApplication
+@EnableConfigurationProperties(RsaKeyProperties.class)
 public class authorizationApplication {
 
 	public static void main(String[] args) {
